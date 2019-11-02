2940b1d56663a8bbf409ebf543cd9360a773ce8e
mochamethod <mochamethod@gmail.com>
2019-11-02 15:55:57 -0600

    12min -- Added console engine header (provided by javid) and set up basic classes and structs for the 3D engine

--- olcEngine3D.cpp
+++ olcEngine3D.cpp
@@ -1,6 +1,51 @@
-#include <iostream>
+#include <iostream> 
+
+#include "olcConsoleGameEngine.h"
+
+using namespace std;
+
+struct vec3d
+{
+	float x, y, z;
+};
+
+struct triangle
+{
+	vec3d p[3];
+};
+
+struct mesh
+{
+	vector<triangle> tris;
+
+};
+
+class olcEngine3D : public olcConsoleGameEngine
+{
+public:
+	olcEngine3D()
+	{
+		m_sAppName = L"3D Demo";
+	}
+
+public:
+	bool OnUserCreate() override
+	{
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		return true;
+	}
+};
 
 int main()
 {
+	olcEngine3D demo;
+	if (demo.ConstructConsole(256, 240, 4, 4)) {
+		demo.Start();
+	}
+
 	return 0;
 }
