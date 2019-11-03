ebbbbe53ba8950cebd86ec0d07f5f7d53628d1e6
mochamethod <mochamethod@gmail.com>
2019-11-02 19:15:01 -0600

    Added rotation to the cube. However, I do not see the rotation nor the cube in the output console window. I will need to investigate further.

--- olcEngine3D.cpp
+++ olcEngine3D.cpp
@@ -36,27 +36,6 @@ public:
 public:
 	bool OnUserCreate() override
 	{
-		return true;
-	}
-
-	bool OnUserUpdate(float fElapsedTime) override
-	{
-		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
-
-		// Draw triangles
-		for (auto tri : meshCube.tris) {
-
-		}
-
-		return true;
-	}
-private:
-	mesh meshCube;
-	mat4x4 matProj;
-
-public:
-	bool OnUserCreate() override
-	{
 		meshCube.tris = {
 			// South
 			{ 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f },
@@ -89,7 +68,7 @@ public:
 		float fFov = 90.0f;
 		float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
 		float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
-		
+
 		matProj.m[0][0] = fAspectRatio * fFovRad;
 		matProj.m[1][1] = fFovRad;
 		matProj.m[2][2] = fFar / (fFar - fNear);
@@ -99,6 +78,99 @@ public:
 
 		return true;
 	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+
+		mat4x4 matRotZ, matRotX;
+		fTheta += 1.0f * fElapsedTime;
+
+		// Rotation Z
+		matRotZ.m[0][0] = cosf(fTheta);
+		matRotZ.m[0][1] = sinf(fTheta);
+		matRotZ.m[1][0] = -sinf(fTheta);
+		matRotZ.m[1][1] = cosf(fTheta);
+		matRotZ.m[2][2] = 1;
+		matRotZ.m[3][3] = 1;
+
+		// Rotation X
+		matRotZ.m[0][0] = 1;
+		matRotZ.m[1][1] = cosf(fTheta * 0.5f);
+		matRotZ.m[1][2] = sinf(fTheta * 0.5f);
+		matRotZ.m[2][1] = -sinf(fTheta * 0.5f);
+		matRotZ.m[2][2] = cosf(fTheta * 0.5f);
+		matRotZ.m[3][3] = 1;
+
+		// Draw triangles
+		for (auto tri : meshCube.tris) {
+			triangle triProjected, triTranslated, triRotatedZ, triRotatedZX;
+
+			MultiplyMatrixVector(tri.p[0], triRotatedZ.p[0], matRotZ);
+			MultiplyMatrixVector(tri.p[1], triRotatedZ.p[0], matRotZ);
+			MultiplyMatrixVector(tri.p[2], triRotatedZ.p[0], matRotZ);
+			
+			MultiplyMatrixVector(triRotatedZ.p[0], triRotatedZX.p[0], matRotX);
+			MultiplyMatrixVector(triRotatedZ.p[1], triRotatedZX.p[0], matRotX);
+			MultiplyMatrixVector(triRotatedZ.p[2], triRotatedZX.p[0], matRotX);
+			
+			triTranslated = tri;
+			triTranslated.p[0].z = triRotatedZX.p[0].z + 3.0f;
+			triTranslated.p[1].z = triRotatedZX.p[1].z + 3.0f;
+			triTranslated.p[2].z = triRotatedZX.p[2].z + 3.0f;
+
+			MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
+			MultiplyMatrixVector(triTranslated.p[1], triProjected.p[0], matProj);
+			MultiplyMatrixVector(triTranslated.p[2], triProjected.p[0], matProj);
+
+			triProjected.p[0].x += 1.0f;
+			triProjected.p[0].y += 1.0f;
+
+			triProjected.p[1].x += 1.0f;
+			triProjected.p[1].y += 1.0f;
+
+			triProjected.p[2].x += 1.0f;
+			triProjected.p[2].y += 1.0f;
+
+			triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[0].y *= 0.5f * (float)ScreenWidth();
+
+			triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[1].y *= 0.5f * (float)ScreenWidth();
+
+			triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[2].y *= 0.5f * (float)ScreenWidth();
+
+			DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+				triProjected.p[1].x, triProjected.p[1].y,
+				triProjected.p[2].x, triProjected.p[2].y,
+				PIXEL_SOLID, FG_WHITE);
+		}
+
+		return true;
+	}
+private:
+	mesh meshCube;
+	mat4x4 matProj;
+
+	float fTheta;
+
+	void MultiplyMatrixVector(vec3d& i, vec3d &o, mat4x4& m)
+	{
+		o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+		o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+		o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+		float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+		
+		if (w != 0.0f) {
+			o.x /= w;
+			o.y /= w;
+			o.z /= w;
+		}
+	}
+
+public:
+
 };
 
 int main()
