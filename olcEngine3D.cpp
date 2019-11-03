350edab949e63e1a9069e1e37419c8e61d2b555a
mochamethod <mochamethod@gmail.com>
2019-11-02 18:38:45 -0600

    30:32 -- Implemented a matrix formula to normalize the project of in-game objects relative to the screen's aspect ratio and the player's field of view.

--- olcEngine3D.cpp
+++ olcEngine3D.cpp
@@ -20,6 +20,11 @@ struct mesh
 
 };
 
+struct mat4x4
+{
+	float m[4][4] = { 0 };
+};
+
 class olcEngine3D : public olcConsoleGameEngine
 {
 public:
@@ -36,6 +41,62 @@ public:
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
+		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+
+		// Draw triangles
+		for (auto tri : meshCube.tris) {
+
+		}
+
+		return true;
+	}
+private:
+	mesh meshCube;
+	mat4x4 matProj;
+
+public:
+	bool OnUserCreate() override
+	{
+		meshCube.tris = {
+			// South
+			{ 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f },
+
+			// East               
+			{ 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f },
+
+			// North              
+			{ 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f },
+
+			// West               
+			{ 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f },
+
+			// Top               
+			{ 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f },
+			{ 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f },
+
+			// Bottom             
+			{ 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f },
+			{ 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f },
+		};
+
+		// Projection matrix
+		float fNear = 0.1f;
+		float fFar = 1000.0f;
+		float fFov = 90.0f;
+		float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
+		float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
+		
+		matProj.m[0][0] = fAspectRatio * fFovRad;
+		matProj.m[1][1] = fFovRad;
+		matProj.m[2][2] = fFar / (fFar - fNear);
+		matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+		matProj.m[2][3] = 1.0f;
+		matProj.m[3][3] = 0.0f;
+
 		return true;
 	}
 };
